64d5c65d4bb3ba66310fd49d3b158a739e49e9b7
Khushbusaifi012 <khushbusaifi27@gmail.com>
2025-05-19 13:23:39 +0530

    to add a new api

--- myvenv/main.py
+++ myvenv/main.py
@@ -1,13 +1,42 @@
 from fastapi import FastAPI
+import json
 
 app=FastAPI()
 
+def load_data():
+    with open(r'C:\Users\FARUKH KHAN\OneDrive\khushbu personal\rest_api\myvenv\patients.json','r') as f:
+        data=json.load(f)
+    return data
+
+
 @app.get("/")
 
-def hello():
+def hello():    #first api
     return {'message':'Hello World !'}
 
 @app.get('/about')
 
-def about():
+def about():   #second api
     return {'message':'Essentia.dev is a software development company !'}
+
+@app.get('/myself')
+
+def myself():    #third api
+    return {
+        'name':'Khushbu saifi',
+        'profile':'Software developer',
+        'office':'Essentia softserv'
+    }
+
+@app.get('/patient')
+def patient():
+    return {'message':'Patient Management System API'}
+
+@app.get('/patients')
+def patients():
+    return {'message':'A fully Functional API to manage your patient records ! '}
+
+@app.get('/view')
+def view():   #fourth api
+    data=load_data()
+    return data
